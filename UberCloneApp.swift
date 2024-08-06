bfd3051ca99c2c89dec83b52401eda528963bc0a
Saiyam Chawla <76040963+Saiyam0710@users.noreply.github.com>
2024-08-06 18:01:55 +0530

    Add files via upload

---
+++ UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Target Integration on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
